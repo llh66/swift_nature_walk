08c07a321005fcabad3668fe6e2fd6edbe956ab2
LLH <llh@Zhaoyangs-MacBook-Air.local>
2025-02-08 13:05:46 -0500

    first commit

---
+++ Group10_Nature_walk/Group10_Nature_walkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group10_Nature_walkApp.swift
+//  Group10_Nature_walk
+//
+//  Created by LLH on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Group10_Nature_walkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
