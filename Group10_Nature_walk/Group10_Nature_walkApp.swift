dbe9528afbb8e8a7f26c342349b55b06e60a659b
davidHuyDang <davidhuydang02@gmail.com>
2025-02-12 15:16:35 -0500

    Update Group10_Nature_walkApp.swift

--- Group10_Nature_walk/Group10_Nature_walkApp.swift
+++ Group10_Nature_walk/Group10_Nature_walkApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct Group10_Nature_walkApp: App {
+    
+    // State object to manage the session state throughout the app
+    @StateObject private var sessionManager = SessionManager()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // Pass the sessionManager object as an environment object to the ContentView
+            ContentView().environmentObject(sessionManager)
         }
     }
 }
