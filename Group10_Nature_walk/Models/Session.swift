ca264c6e6a57714d31385a210a2b958e8a5852ed
davidHuyDang <davidhuydang02@gmail.com>
2025-02-13 01:15:48 -0500

    Update Session.swift

--- Group10_Nature_walk/Models/Session.swift
+++ Group10_Nature_walk/Models/Session.swift
@@ -17,7 +17,7 @@ class Session: Identifiable, Codable {
     let photos: [String]
     let pricePerPerson: Double
     
-// Initializer to set up a session with a name, price, and photo
+// Initializer to set up a session 
     init(name: String, description: String, starRating: Int, host: String, photos: [String], pricePerPerson: Double) {
         self.name = name
         self.description = description
